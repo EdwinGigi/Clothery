b2d8235497b65de58c2908083f40556adf232e29
Bogdan Eliseev <P2414555@my365.dmu.ac.uk>
2020-03-07 15:11:15 +0000

    New chnages to connect to database

--- Clothery_Classes/clsOrder.cs
+++ Clothery_Classes/clsOrder.cs
@@ -4,10 +4,96 @@ namespace Clothery_Classes
 {
     public class clsOrder
     {
-        public bool IsPaid;
-        public DateTime OrderDate;
-        public int OrderId;
-        public int CustomerId;
-        public string ShippingAddress;
+        private Int32 mOrderId;
+        private DateTime mOrderDate;
+        private Int32 mCustomerId;
+        private Boolean mIsPaid;
+        private String mShippingAddress;
+
+
+        public bool IsPaid
+        {
+            get
+            {
+                return mIsPaid;
+            }
+            set
+            {
+                mIsPaid = value;
+            }
+        }
+        public DateTime OrderDate
+        {
+            get
+            {
+                return mOrderDate;
+            }
+            set
+            {
+                mOrderDate = value;
+            }
+        }
+        
+        public Int32 OrderId
+        {
+               get
+            {
+                return mOrderId;
+            }
+            set
+            {
+                mOrderId = value;
+            }
+        }
+
+        public Int32 CustomerId
+        {
+            get
+            {
+                return mCustomerId;
+            }
+            set
+            {
+                mCustomerId = value;
+            }
+        }
+        public string ShippingAddress
+        {
+            get
+            {
+                return mShippingAddress;
+            }
+            set
+            {
+                mShippingAddress = value;
+            }
+        }
+
+
+        public bool Find(int OrderId)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderId", OrderId);
+            DB.Execute("sproc_tbl_Order_FilterByOrderId");
+
+            if(DB.Count ==1)
+            {
+                mOrderId = Convert.ToInt32(DB.DataTable.Rows[0]["OrderId"]);
+                mOrderDate = Convert.ToDateTime(DB.DataTable.Rows[0]["OrderDate"]);
+                mCustomerId = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerId"]);
+                mIsPaid = Convert.ToBoolean(DB.DataTable.Rows[0]["IsPaid"]);
+                mShippingAddress = Convert.ToString(DB.DataTable.Rows[0]["ShippingAddress"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+   
+
     }
+  
+    
 }
--- Clothery_Classes/clsOrderLine.cs
+++ Clothery_Classes/clsOrderLine.cs
@@ -1,14 +1,110 @@
-﻿namespace Clothery_Classes
+﻿using System;
+namespace Clothery_Classes
 {
     public class clsOrderLine
     {
-        public bool Shipped;
-        public string Description;
-        public string Size;
-        public double Price;
-        public int Quantity;
-        public int ProductId;
-        public int OrderId;
-        public int OrderLineId;
+        private Boolean mShipped;
+        private String mDescription;
+        private String mSize;
+        private Double mPrice;
+        private Int32 mQuantity;
+        private Int32 mProductId;
+        private Int32 mOrderId;
+        private Int32 mOrderLineId;
+
+        public bool Shipped
+        {
+            get
+            {
+                return mShipped;
+            }
+            set
+            {
+                mShipped = value;
+            }
+        }
+
+        public string Description
+        {
+            get 
+            {
+                return mDescription;
+
+            }
+            set
+            {
+                mDescription = value;
+            }
+        }
+        public string Size
+        {
+            get
+            {
+                return mSize;
+            }
+            set
+            {
+                mSize = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+
+        public Int32 Quantity
+        {
+            get
+            {
+                return mQuantity;
+            }
+            set
+            {
+                mQuantity = value;
+            }
+        }
+        public Int32 ProductId
+        {
+            get
+            {
+                return mProductId;
+            }
+            set
+            {
+                mProductId = value;
+            }
+        }
+        public Int32 OrderId
+        {
+            get
+            {
+                return mOrderId;
+            }
+            set
+            {
+                mOrderId = value;
+            }
+        }
+        public Int32 OrderLineId
+        {
+            get
+            {
+                return mOrderLineId;
+            }
+            set
+            {
+                mOrderLineId = value;
+            }
+        }
+
+
+
     }
 }
