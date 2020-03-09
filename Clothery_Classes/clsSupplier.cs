dad3bfb242a9027494e74c2818e0c8f0e1d3f356
Edwin Gigi <P2508298@my365.dmu.ac.uk>
2020-03-09 17:39:29 +0000

    Week 19 - errors need fixing.

--- Clothery_Classes/clsSupplier.cs
+++ Clothery_Classes/clsSupplier.cs
@@ -1,6 +1,15 @@
-﻿namespace Clothery_Classes
+﻿using System;
+
+namespace Clothery_Classes
 {
     public class clsSupplier
     {
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierAddress { get; set; }
+        public string SupplierPostCode { get; set; }
+        public string Description { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public bool StockAvailable { get; set; }
     }
 }
---
+++ Clothery_Front_Office/SupplierViewer.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SupplierViewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //create a new instance of clsSupplier
+        clsSupplier AnSupplier = new clsSupplier();
+        //get the data from the session object
+        AnSupplier = (clsSupplier)Session["AnSupplier"];
+        //display the supplier id for this entry
+        Response.Write(AnSupplier.SupplierId);
+    }
+}
