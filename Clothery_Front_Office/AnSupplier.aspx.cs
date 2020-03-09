156c68d89355195eb151994a9da9c6922e9dc363
Edwin Gigi <P2508298@my365.dmu.ac.uk>
2020-03-09 17:42:59 +0000

    Week 19 - Errors need fixing

---
+++ Clothery_Front_Office/AnSupplier.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AnSupplier : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnSubmit_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsSupplier
+        clsSupplier AnSupplier = new clsSupplier();
+        //capture the Supplier ID no.
+        AnSupplier.SupplierId = intSupplierId.Int32;
+        //store the id in the session object
+        Session["AnSupllier"] = AnSupplier;
+        //redirect to the viewer page
+        Response.Redirect("AddressViewer.aspx");
+    }
+}
